f1bffe4eb21e1faec3227f37e91dd6291b29cc41
st <jiju046@gmail.com>
2023-09-14 19:17:48 +0530

    navbar

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -1,50 +1,61 @@
-@extends('layouts.book')
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
 
-@section('content')
-<div class="container">
-    <h1>Book List</h1>
-    @if(session('success'))
-        <div class="alert alert-success">
-            {{ session('success') }}
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <div class="container">
+                        <h1>Book List</h1>
+                        @if(session('success'))
+                            <div class="alert alert-success">
+                                {{ session('success') }}
+                            </div>
+                        @endif
+
+                        <table class="table table-striped">
+                            <thead>
+                                <tr>
+                                    <th>Title</th>
+                                    <th>Available Days</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+
+                            <tbody>
+                                @forelse($books as $book)
+                                    <tr>
+                                        <td>{{ $book->title }}</td>
+                                        <td>{{ $book->available_days }}</td>
+                                        <td>
+                                            <a href="{{ route('books.show', $book->id) }}" class="btn btn-info">View</a>
+                                            <a href="{{ route('books.edit', $book->id) }}" class="btn btn-primary">Edit</a>
+                                            {!! Form::open(['route' => ['books.destroy', $book->id], 'method' => 'DELETE', 'style' => 'display: inline;']) !!}
+                                                {!! Form::token() !!}
+                                                {!! Form::submit('Delete', ['class' => 'btn btn-danger', 'onclick' => 'return confirm("Are you sure you want to delete this book?")']) !!}
+                                            {!! Form::close() !!}
+                                        </td>
+                                    </tr>
+                                @empty
+                                    <tr>
+                                        <td colspan="3" class="text-center">No Records Found</td>
+                                    </tr>
+                                @endforelse
+                            </tbody>
+                        </table>
+
+                        <a href="{{ route('books.create') }}" class="btn btn-success">Add Book</a>
+                    </div>
+
+                    <div>
+                        <a href="{{ route('dashboard') }}" class="btn btn-primary my-5">Dashboard</a>
+                    </div>
+                </div>
+            </div>
         </div>
-    @endif
-
-    <table class="table table-striped">
-        <thead>
-            <tr>
-                <th>Title</th>
-                <th>Available Days</th>
-                <th>Action</th>
-            </tr>
-        </thead>
-
-        <tbody>
-            @forelse($books as $book)
-                <tr>
-                    <td>{{ $book->title }}</td>
-                    <td>{{ $book->available_days }}</td>
-                    <td>
-                        <a href="{{ route('books.show', $book->id) }}" class="btn btn-info">View</a>
-                        <a href="{{ route('books.edit', $book->id) }}" class="btn btn-primary">Edit</a>
-                        {!! Form::open(['route' => ['books.destroy', $book->id], 'method' => 'DELETE', 'style' => 'display: inline;']) !!}
-                            {!! Form::token() !!}
-                            {!! Form::submit('Delete', ['class' => 'btn btn-danger', 'onclick' => 'return confirm("Are you sure you want to delete this book?")']) !!}
-                        {!! Form::close() !!}
-
-                    </td>
-                </tr>
-            
-            @empty
-                
-            <td colspan="3" class="text-center">No Records Found</td>
-            @endforelse
-        </tbody>
-    </table>
-    
-    <a href="{{ route('books.create') }}" class="btn btn-success">Add Book</a>
-</div>
-
-<div>
-    <a href="{{ route('dashboard') }}" class="btn btn-primary my-5">Dashboard</a>
-</div>
-@endsection
+    </div>
+</x-app-layout>
